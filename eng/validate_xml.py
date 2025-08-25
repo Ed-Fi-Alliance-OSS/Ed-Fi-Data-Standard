9c485b1d24a6dea379a3b1f080858a89f42e4dba
Stephen A. Fuqua <stephen@safnet.com>
2025-08-25 08:56:54 -0500

    Apply file headers

--- eng/validate_xml.py
+++ eng/validate_xml.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
 """
 XML Validation Script for Ed-Fi Data Standard
 
